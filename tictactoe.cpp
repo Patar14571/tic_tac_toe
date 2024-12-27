a1f8ddc9522d465f69f3b2bd63617d43b2e8245c
Patrick Stevenson <patarjsteve@gmail.com>
2024-12-27 13:41:30 -0500

    Got rid of old comment

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,22 +1,7 @@
 #include "game.hpp"
 
-/*
-Things to do for tic-tac-toe:
-1. Collect user input
-    a. parse input for row and column
-    b. feedback on values that are not allowed
-2. Display user X
-    a. translate the entered value to the grid
-    b. add CPU to the grid
-    c. print the grid
-3. Create a simple algo for the CPU
-    a. CPU algo: Place into first available position
-
-*/
-
 main() {
     Game game;
     game.run();
     return 0;
 }
-
